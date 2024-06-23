1afb893d931601b7f69f2ca95b5210e8ad1a61b1
算神 <lishewen@msn.com>
2024-06-23 18:45:38 +0800

    添加项目文件。

---
+++ SK.Ollama.Qwen2/OllamaChatCompletionService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel;
+using OllamaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK.Ollama.Qwen2
+{
+    public class OllamaChatCompletionService : IChatCompletionService
+    {
+        public IReadOnlyDictionary<string, object?> Attributes => throw new NotImplementedException();
+
+        public async Task<IReadOnlyList<ChatMessageContent>> GetChatMessageContentsAsync(ChatHistory chatHistory, PromptExecutionSettings? executionSettings = null, Kernel? kernel = null, CancellationToken cancellationToken = default)
+        {
+            var ollama = new OllamaApiClient("http://localhost:11434", "qwen2");
+
+            var chat = new Chat(ollama, _ => { });
+
+            var lastMessage = chatHistory.LastOrDefault();
+
+            string question = lastMessage?.Content!;
+            var chatResponse = "";
+            var history = (await chat.Send(question, CancellationToken.None)).ToArray();
+
+            var last = history.Last();
+            chatResponse = last.Content;
+
+            return [new(AuthorRole.Assistant, chatResponse)];
+        }
+
+
+        public IAsyncEnumerable<StreamingChatMessageContent> GetStreamingChatMessageContentsAsync(ChatHistory chatHistory, PromptExecutionSettings? executionSettings = null, Kernel? kernel = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SK.Ollama.Qwen2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel;
+using SK.Ollama.Qwen2;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddKeyedSingleton<IChatCompletionService>("ollamaChat", new OllamaChatCompletionService());
+var kernel = builder.Build();
+
+string prompt = "你是谁？";
+var response = await kernel.InvokePromptAsync(prompt);
+Console.WriteLine(response.GetValue<string>());
